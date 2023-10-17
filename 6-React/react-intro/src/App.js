3ba3312edbd06da0f76aefb938b9e0b60d4c8b95
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2023-10-17 20:47:35 +0530

    react-demo

---
+++ 6-React/react-intro/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to React.js</h2>
+      <hr />
+      <p>This is a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
